4dc8d92124db2a70d0b96850f43bb32a3b0dbfe4
PaulaG9 <paula.ghergu@yahoo.ro>
2020-12-08 17:57:11 +0000

    commit

---
+++ calc/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+from django_select2 import forms as s2forms
+
+from calc.models import Pharmacy
--- calc/models.py
+++ calc/models.py
@@ -7,10 +7,9 @@ class Scenario(models.Model):
     scenario_id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     scenario_name=models.CharField(max_length=255, blank=True, null=True)
     description=models.TextField(blank=True, null=True)
-    disease_6m_5yr=models.DecimalField(max_digits=5, decimal_places=4, blank=True, null=True) 
-    disease_6m_15yr=models.DecimalField(max_digits=5, decimal_places=4, blank=True, null=True)
-
-    #also include order lead time?
+    disease_6m_5yr=models.FloatField(default=None, blank=True, null=True) 
+    disease_6m_15yr=models.FloatField(default=None, blank=True, null=True)
+    order_lead_time=models.FloatField(blank=True, null=True)
 
     def __str__(self):
         return str(self.scenario_name)
@@ -26,12 +25,12 @@ class Disease(models.Model):
     scenario=models.ForeignKey(Scenario, on_delete=models.PROTECT)
     disease_name=models.CharField(max_length=255, blank=True, null=True)
     disease_alternative_names=models.CharField(max_length=255, blank=True, null=True)
-    disease_attack_rate=models.DecimalField(max_digits=5, decimal_places=4, blank=True, null=True)
-    disease_hospitalisation_cases=models.DecimalField(max_digits=5, decimal_places=4, blank=True, null=True)
+    disease_attack_rate=models.FloatField(blank=True, null=True)
+    disease_hospitalisation_cases=models.FloatField(blank=True, null=True)
     hospitalisation_length_stay=models.IntegerField(blank=True, null=True)
-    peak_attack_rate=models.DecimalField(max_digits=5, decimal_places=4, blank=True, null=True)
-    incidence=models.DecimalField(max_digits=5, decimal_places=4, blank=True, null=True)
-    prevalence=models.DecimalField(max_digits=5, decimal_places=4, blank=True, null=True)
+    peak_attack_rate=models.FloatField(blank=True, null=True)
+    incidence=models.FloatField(blank=True, null=True)
+    prevalence=models.FloatField(blank=True, null=True)
 
     def __str__(self):
         return str(self.disease_name)
@@ -39,47 +38,34 @@ class Disease(models.Model):
 class Pharmacy(models.Model):
     #define attributes here
     medicine_id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    medicine_code=models.CharField(max_length=50)
-    medicine_name=models.CharField(max_length=10000, null=True)
-    disease_medicine=models.ManyToManyField(Disease)
-    frequency=models.IntegerField()
-    attrition_rate=models.FloatField()
-    patients=models.IntegerField()
-    six_mth_consumption=models.IntegerField()
-    three_mth_consumption=models.IntegerField(blank=True, null=True)
-    last_order=models.IntegerField()  
-    flag2=models.BooleanField(default=False)
-   
-    #also include historic data fields
+    msf_code=models.CharField(max_length=50, verbose_name='MSF Code', null=True, blank=True)
+    medicine_name=models.CharField(max_length=10000, null=True, verbose_name='Description')
+    disease_medicine=models.ManyToManyField(Disease, verbose_name='Condition')
+    frequency=models.FloatField(default=1)
+    attrition_rate=models.FloatField(default=0)
+    min_dosage=models.FloatField(null=True, blank=True)
+    max_dosage=models.FloatField(null=True, blank=True)
+    num_patients=models.FloatField(verbose_name='Number of Patients', default=1)
+    essential_item=models.BooleanField(verbose_name='Essential NCD Item', null=True, blank=True)
+    category=models.CharField(max_length=10000, choices=[('1','Medication'), ('2','Medical Equipment'), ('3','Medical Consumables'), ('4', 'Lab Equipment'), ('5','Lab Consumables')], blank=True, null=True)
+    comments=models.CharField(max_length=10000, blank=True)
     
+    
+    class Meta:
+        verbose_name_plural='Pharmacies'
+
     @property
     def forecasted_amount(self):
-        return self.frequency*self.patients * (1-self.attrition_rate)
+        if self.frequency==0  or self.num_patients==0 or self.attrition_rate==0:
+            return 0
+        else:
+            try:
+                return self.frequency*self.num_patients * (1-self.attrition_rate)
+            except ValueError as e:
+                print (e)
+            
 
     def __str__(self):
         return str(self.medicine_name)
-        
-    # added method to allow consumptioon comparison - not sure how this should be incorporated into forecasted_amount
-    # maybe we need to first compare consumption and then add a correction factor to the forecast as opposed to the other way around
-
-    @property
-    def flagger(self):
-        self.flag=False
-        if self.forecasted_amount/self.six_mth_consumption>1:
-            #set flag to True - pop up a banner that says "check consumption tool":
-            self.flag=True
-        elif self.forecasted_amount/self.three_mth_consumption>1:
-            #...do something else
-            pass
-
-        else:
-            # do something more
-            pass
-        return self.flag
-
-    flag2=flagger
-    
-class Patient(models.Model):
-    # 
-    pass
+  
 
