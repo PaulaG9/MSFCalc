0a327736637a2b35b1063d0636cfdd8181b865b0
PaulaG9 <paula.ghergu@yahoo.ro>
2021-02-10 00:42:48 +0000

    layout and function change

--- calc/static/js/resultsPreview.js
+++ calc/static/js/resultsPreview.js
@@ -14,22 +14,23 @@ function estResults(net_patients, duration, frequency){
 
 function getPreview(){
     
-    var formInst=$('[id^=num_]')
+    var formInst=$('input[id^=duration_]')
     
     for (var x=1; x<formInst.length+1; x++){
-        var numpatients=parseInt($('#num_patients_' +x).val()); 
-        console.log(numpatients);
-        var duration=parseInt($('#duration_' +x).val()); 
+        
+        var duration=parseInt($('input#duration_' +x).val()); 
         console.log(duration);
-        var monincrease=parseInt($('#monthly_increase_' +x).val()); 
+        var monincrease=parseInt($('input#monthly_increase_' +x).val()); 
         console.log(monincrease);
         var results=$('[id^=result_'+ x +']')
         for (var i=1; i<results.length+1; i++){ 
             console.log(i-1, results.eq(i-1))           
             var txt=results.eq(i-1).attr("id")
             var msfcode=txt.split("_")[2]            
-            var frequency=parseInt($('#frequency_'+ x +'_' + msfcode).val()); 
-            console.log(frequency);   
+            var frequency=parseInt($('select#frequency_'+ x +'_' + msfcode).val()); 
+            console.log(frequency);
+            var numpatients=parseInt($('input#num_patients_' + x +'_' + msfcode).val()); 
+            console.log(numpatients);   
 
             estimate=estResults(netPatients(numpatients, duration, monincrease),duration, frequency);                 
             console.log(estimate);
